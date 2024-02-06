88dc07a1d86a2fe0ed975d64c1828a842245bf90
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-06 22:27:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-//your JS code here. If required.
-function checkCookies() {
-	console.log('DOM load success');
-}
+document.addEventListener('DOMContentLoaded', function() {
+  // Create a new paragraph element
+  var paragraph = document.createElement('p');
+  
+  // Set its text content to "DOM load success"
+  paragraph.textContent = 'DOM load success';
+  
+  // Append the paragraph element to the body of the document
+  document.body.appendChild(paragraph);

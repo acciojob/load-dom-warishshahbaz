b5d5296919130229c9bc2b0967c27dc4c12e4928
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-06 23:34:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Create a new paragraph element
-  var paragraph = document.createElement('p');
-  
-  // Set its text content to "DOM load success"
-  paragraph.textContent = 'DOM load success';
-  
-  // Append the paragraph element to the body of the document
-  document.body.appendChild(paragraph);
+window.onload = (event) => {
+  console.log("DOM load success");
+};
